299646f4845b1764be44fb39ebb3afaafddb3db6
Mark Medrano <markumed1@hotmail.com>
2022-07-31 13:19:08 -1000

    commit

---
+++ src/components/app-contents/AppContents.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppContents = () => {
+    return (
+        <div className="bg-sky-400 app-contents-grid">
+            Contents
+        </div>
+    );
+}
+
+export default AppContents
