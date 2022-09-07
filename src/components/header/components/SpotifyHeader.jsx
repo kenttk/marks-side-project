806e6551058b4253ad693d6913dc47844e6db302
Mark Medrano <markumed1@hotmail.com>
2022-09-06 15:16:04 -1000

    Initial Icon Header

---
+++ src/components/header/components/SpotifyHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SpotifyIcon from "../../../assets/icons/SpotifyIcon";
+
+const SpotifyHeader = () => {
+    return (
+        <div className="spotify-logo text-white pt-4 pl-4 ">
+            <SpotifyIcon />
+            
+        </div>
+    );
+}
+
+export default SpotifyHeader;
--- src/components/side-nav/SideNav.jsx
+++ src/components/side-nav/SideNav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SideNav = () => {
     return (
-        <nav className="bg-yellow-500 sidebar-grid">
+        <nav className="bg-black sidebar-grid">
             Side Navigator
         </nav>
     );
