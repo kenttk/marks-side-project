6f3078b5b5ae33c9edbfbc4ce88f42f08ab8a8b5
Mark Medrano <markumed1@hotmail.com>
2022-09-10 13:21:44 -1000

    Update SideNavItem.jsx
    committing update

--- src/components/side-nav/components/SideNavItem.jsx
+++ src/components/side-nav/components/SideNavItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SideNavItem = ({ title, icon }) => {
     return ( 
-        <div className="flex items-center fill-white text-white pb-2 px-4 cursor-pointer gap-2.5">
+        <div className="flex items-center fill-[#b3b3b3] text-[#b3b3b3] pb-2 px-4 cursor-pointer gap-2.5 hover:text-white hover:fill-white">
             {icon} {title}
         </div>
     );
