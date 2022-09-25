ff843cfdaaed3fa787fcfca883b77a394ba81f2f
Mark Medrano <markumed1@hotmail.com>
2022-09-24 19:38:06 -1000

    Update SideNavItem.jsx
    commit for code review.

--- src/components/side-nav/components/SideNavItem.jsx
+++ src/components/side-nav/components/SideNavItem.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
-/*is an element that lets the user navigate to another page by clicking or tapping on it.
+/* is an element that lets the user navigate to another page by clicking or tapping on it.
 In react-router-dom, a <Link> renders an accessible <a> element with a real href that points 
 to the resource it's linking to. */
 
-const SideNavItem = ({ title, icon, route }) => {
+/* This hook returns the current 'location' object. This can be useful if
+you'd like to perform some side effect whenver the current location changes. */
+import { useLocation } from "react-router-dom";
+
+const SideNavItem = ({ title, icon, route  }) => {
+    let location = useLocation();
+
+    console.log(location.pathname);
+    
+    
+    /* Adding logic 'IF' and 'ELSE' statements to make sure we set 
+    the correct css classes inside of the component(buidling blocks
+    of any React app). */
+    const getClassName = () => {
+        if (location.pathname === route) {
+            return "flex items-center fill-[#FFFFFF] text-[#FFFFFF] pb-2 px-4 cursor-pointer gap-2.5 hover:text-white hover:fill-white"
+            //returns class names to make text white.
+        } else {
+            return "flex items-center fill-[#b3b3b3] text-[#b3b3b3] pb-2 px-4 cursor-pointer gap-2.5 hover:text-white hover:fill-white"
+            //returns class names to make text grey.
+        }
+    }; 
+    
     return ( 
         <Link
-            className="flex items-center fill-[#b3b3b3] text-[#b3b3b3] pb-2 px-4 cursor-pointer gap-2.5 hover:text-white hover:fill-white"
+            className={getClassName()}
             to={route}
         >
             {icon} {title}
