9df49a733c3493c6dac62de51e7be5fc9c9bc655
Mark Medrano <markumed1@hotmail.com>
2022-10-14 12:31:45 -1000

    Update ButtonPill.jsx
    OnClick. Review code.

--- src/components/button-pill/ButtonPill.jsx
+++ src/components/button-pill/ButtonPill.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 const ButtonPill = ({text}) => {
+    const handleClick = () => {
+        console.log('City Boy Villains');
+    }
+
     return (
-        <button className="bg-white transform motion-safe:hover:scale-110 text-black font-24px font-bold py-2 px-4 rounded-full py-2 px-8">
+        <button onClick={handleClick} className="bg-white transform motion-safe:hover:scale-110 text-black font-24px font-bold py-2 px-4 rounded-full py-2 px-8">
             {text}
         </button>
     );
