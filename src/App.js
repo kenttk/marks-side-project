763f3b4ec9798b3591d4c405c2d04dfe1340c549
Mark Medrano <markumed1@hotmail.com>
2022-11-02 14:07:24 -1000

    Code review
    login link

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ import SideNav from "./components/side-nav/SideNav";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import Search from "./pages/search/Search";
+import Login from "./pages/login/Login";
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation()
+  const shouldShowHeaderAndSidenav = location.pathname !== "/login";
+
   return (
-    <main className="grid-container">
-      <Header />
-      <SideNav />
-      <div className="app-contents-grid">
+    <main className={shouldShowHeaderAndSidenav ? "grid-container" : undefined}>
+      {shouldShowHeaderAndSidenav && <Header />}
+      {shouldShowHeaderAndSidenav && <SideNav />}
+      <div className={shouldShowHeaderAndSidenav ? "app-contents-grid" : undefined}>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/search" element={<Search/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </div>
     </main>
