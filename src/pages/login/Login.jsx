b1dfa63941f9503fd03819bb7cf507bc45b37794
Mark Medrano <markumed1@hotmail.com>
2022-11-16 14:29:18 -1000

    login
    spotify logo in login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import SpotifyIcon from "../../assets/icons/SpotifyIcon";
+import "./login.css";
+
 const Login = () => {
-    return (
-        <h1>Login</h1>
-    );
-}
+  return (
+    <div className="px-2 py-3">
+      <div className="login__spotify-logo">
+          <SpotifyIcon />
+      </div>
+      
+      <hr />
+
+    </div>
+  );
+};
 
 export default Login;
