ccf05443720047ad8d533f384bb17d1575602da6
Kent Kawahara <kkawahara@slack-corp.com>
2023-01-06 16:37:55 -0800

    work in progress

--- src/pages/login/InputField.jsx
+++ src/pages/login/InputField.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
-
-
-const InputField = ({label, placeholder, type}) => {
+const InputField = ({
+    label,
+    placeholder,
+    type,
+    value,
+    onChange,
+    errorMessage,
+}) => {
     return (
             <label className="flex flex-col text-sm w-full">
                 {label}
                 <input
-                    className="border-2 border rounded border-black p-3.5"
+                    // ${errorMessage ? 'border-rose-500' : 'border-black'} is a ternary statement. Basically it's a fancy
+                    // word and shortcut for doing an if/else statement
+                    // if (errorMessage) {
+                    //  set the border to red
+                    // } else {
+                    //  set the border to black
+                    // }
+                    className={`border-2 border rounded p-3.5 ${errorMessage ? 'border-rose-500' : 'border-black'}`}
                     type={type || "text"}
                     placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
                 />
             </label>
     );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 import SpotifyIcon from "../../assets/icons/SpotifyIcon";
-import "./login.css";
+import { useState } from 'react';
+
 import InputField from "./InputField";
+import "./login.css";
 
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const emailOnChange = (event) => {
+    setEmail(event.target.value);
+    const inputFieldIsEmpty = event.target.value === '';
+
+    if (inputFieldIsEmpty) {
+      setEmailError('Please enter your Spotify username or email address');
+    } else {
+      setEmailError('');
+    }
+  }
+
   return (
     <div className="px-2 py-3">
       <div className="login__spotify-logo">
@@ -19,6 +35,9 @@ const Login = () => {
             <InputField
               label="Email address or username" 
               placeholder="Email address or username"
+              value={email}
+              onChange={emailOnChange}
+              errorMessage={emailError}
             />
         </div>
               <InputField
