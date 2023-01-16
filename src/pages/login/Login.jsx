7d98edc445754fc17575b35d6cea35be5f93ae25
Mark Medrano <markumed1@hotmail.com>
2023-01-16 09:10:52 -1000

    commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,72 +1,67 @@
 import React, { useState } from "react";
 
 import SpotifyIcon from "../../assets/icons/SpotifyIcon";
-import { useState } from 'react';
 
 import InputField from "./InputField";
 import "./login.css";
 
-
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailErorr] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailErorr] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const emailOnChange = (event) => {
     setEmail(event.target.value);
-    const inputFieldIsEmpty = event.target.value === '';
+    const inputFieldIsEmpty = event.target.value === "";
 
     if (inputFieldIsEmpty) {
-      setEmailErorr ('Please enter your Spotify username or email address');
+      setEmailErorr("Please enter your Spotify username or email address");
     } else {
-      setEmailErorr('');
+      setEmailErorr("");
     }
-  }
+  };
 
   const passwordOnChange = (event) => {
     setPassword(event.target.value);
-    const inputFieldIsEmpty = event.target.value === '';
+    const inputFieldIsEmpty = event.target.value === "";
 
     if (inputFieldIsEmpty) {
-      setPasswordError ('Please enter your password');
+      setPasswordError("Please enter your password");
     } else {
-      setPasswordError('');
+      setPasswordError("");
     }
-  }
+  };
 
   return (
     <div className="px-2 py-3">
       <div className="login__spotify-logo">
-          <SpotifyIcon />
+        <SpotifyIcon />
       </div>
-      
+
       <hr />
-    <div className="flex justify-center">
-      <form className="login_form">
-        <div className="mb-4 w-full">
+      <div className="flex justify-center">
+        <form className="login_form">
+          <div className="mb-4 w-full">
             <InputField
-              label="Email address or username" 
+              label="Email address or username"
               placeHolder="Email address or username"
               value={email}
               onChange={emailOnChange}
               errorMessage={emailError}
             />
-        </div>
-              <InputField
-                label="Password" 
-                placeHolder="Password"
-                type="password"
-                value={password}
-                onChange={passwordOnChange}
-                errorMessage={passwordError}
-
-              />
+          </div>
+          <InputField
+            label="Password"
+            placeHolder="Password"
+            type="password"
+            value={password}
+            onChange={passwordOnChange}
+            errorMessage={passwordError}
+          />
         </form>
-    </div>
-      <div>
-
       </div>
+      <div></div>
     </div>
   );
 };
