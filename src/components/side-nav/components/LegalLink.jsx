b0a6a828b370cd16d591904d04e73c4c40aa5178
Mark Medrano <markumed1@hotmail.com>
2023-03-15 15:16:27 -1000

    Final
    Login Auth

--- src/components/side-nav/components/LegalLink.jsx
+++ src/components/side-nav/components/LegalLink.jsx
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
 
 const LegalLink = ({ href, title }) => {
-    return (
-        <a href={href} target="_blank"><span class="text-gray-400 text-[11px] no-underline hover:underline">{title}</span></a>
-    );
-}
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      <span className="text-gray-400 text-[11px] no-underline hover:underline">
+        {title}
+      </span>
+    </a>
+  );
+};
 
 export default LegalLink;
